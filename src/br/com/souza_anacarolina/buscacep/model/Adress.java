f176a435740feacde1af00064fa2da354101b8b9
Ana <souzacarol874@gmail.com>
2026-05-23 00:46:20 -0300

    Esboço inicial do projeto

---
+++ src/br/com/souza_anacarolina/buscacep/model/Adress.java
@@ -0,0 +1,39 @@
+package br.com.souza_anacarolina.buscacep.model;
+
+public class Adress {
+
+    String cep;
+    String logradouro;
+    String bairro;
+    Boolean erro;
+
+    public Boolean getErro() {
+        return erro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+
+}
